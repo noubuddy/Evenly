6e668dae1e77b6c62b8eaf6ac0f5cd6cfaf5a535
noubuddy <anton.nitsiportsik2@tptlive.ee>
2022-03-16 10:50:13 +0200

    add registration with Jwt-tokens

--- Evenly/Controllers/AuthController.cs
+++ Evenly/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Evenly.Models;
 using System.Security.Cryptography;
-using Evenly.Contexts;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+
+#nullable disable
 
 namespace Evenly.Controllers
 {
@@ -10,16 +14,16 @@ namespace Evenly.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
 
-        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt) {
-            using (var hmac = new HMACSHA512()) {
-                passwordSalt = hmac.Key;
-                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
-            }
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
-        public IActionResult Register(UserDto request){
+        public IActionResult Register(UserDto request)
+        {
             CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
 
             user.Username = request.Username;
@@ -30,13 +34,47 @@ namespace Evenly.Controllers
         }
 
         [HttpPost("login")]
-        public IActionResult Login(UserDto requset){
-
-            if (user.Username != requset.Username) {
+        public IActionResult Login(UserDto requset)
+        {
+            if (user.Username != requset.Username)
                 return BadRequest("User not found.");
-            }
 
-            return Ok("Success");
+            if (!VerifyPasswordHash(requset.Password, user.PasswordHash, user.PasswordSalt))
+                return BadRequest("Wrong password");
+
+            string token = CreateToken(user);
+            return Ok(token);
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computeHash.SequenceEqual(passwordHash);
+            }
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {new Claim(ClaimTypes.Name, user.Username)};
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(claims: claims, expires: DateTime.Now.AddDays(1), signingCredentials: creds);
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
